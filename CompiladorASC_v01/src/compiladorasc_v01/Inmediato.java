e481f8781bfa3f4ade929971632a32e6304122b9
NiverMtz <nivermartinezmed@gmail.com>
2019-09-23 23:53:33 -0500

    Estructura del Proyecto, primera parte

---
+++ CompiladorASC_v01/src/compiladorasc_v01/Inmediato.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package compiladorasc_v01;
+
+/**
+ *
+ * @author niver
+ */
+public class Inmediato {
+    
+}
