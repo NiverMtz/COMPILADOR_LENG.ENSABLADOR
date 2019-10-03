50509e37616c8e409bf46ab193d6e6cd7c4c1d18
NiverMtz <nivermartinezmed@gmail.com>
2019-10-03 02:08:43 -0500

    Estructura del Proyecto, primera parte

--- CompiladorASC_v01/src/compiladorasc_v01/CompiladorASC_v01.java
+++ CompiladorASC_v01/src/compiladorasc_v01/CompiladorASC_v01.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
+import java.util.regex. *;
 /* @author niver */
 
 public class CompiladorASC_v01 {
@@ -12,8 +12,17 @@ public class CompiladorASC_v01 {
    
     public static void main(String[] args) {
         
-        File file = new File("EXEMPLO.LST");
-        try {
+        //File file = new File("EXEMPLO.LST");
+        
+        String input = " LDAA #$25";
+
+        Inmediato.reconocerIMM(input);
+        
+        }
+    }
+        
+        
+        /*try {
             
             fileProcessor process= new fileProcessor(file);
             process.processBuffer();
@@ -21,8 +30,6 @@ public class CompiladorASC_v01 {
         } catch (FileNotFoundException ex) {
             Logger.getLogger(CompiladorASC_v01.class.getName()).log(Level.SEVERE, null, ex);
             System.err.print("The file couldn't found");
-        }
-        
-    }
+        }*/
     
-}
+//}
--- CompiladorASC_v01/src/compiladorasc_v01/Inmediato.java
+++ CompiladorASC_v01/src/compiladorasc_v01/Inmediato.java
@@ -4,11 +4,23 @@
  * and open the template in the editor.
  */
 package compiladorasc_v01;
+import java.util.regex.*;
 
 /**
  *
  * @author niver
  */
 public class Inmediato {
+    
+    public static void reconocerIMM(String input){
+        Pattern p;
+        Matcher m;
+        p = Pattern.compile("#");
+        m = p.matcher(input);
+
+        if (m.find()) {
+            System.out.println("Modo inmediato");
+        }
+    } 
     
 }
