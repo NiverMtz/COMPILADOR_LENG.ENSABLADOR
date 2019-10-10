d08ca6e5653a65b7847b793710cf6a4fbf138913
NiverMtz <nivermartinezmed@gmail.com>
2019-10-10 03:45:34 -0500

    Hay dos main ¿Qué pedo?

--- CompiladorASC_v01/src/compiladorasc_v01/CompiladorASC_v01.java
+++ CompiladorASC_v01/src/compiladorasc_v01/CompiladorASC_v01.java
@@ -12,7 +12,7 @@ public class CompiladorASC_v01 {
 
     public static void main(String[] args) {
 
-        File file = new File("EXEMPLO.ASC");
+        /*File file = new File("EXEMPLO.ASC");
         int i = 1;
 
         //String input = " LDAA #$25";
@@ -30,7 +30,30 @@ public class CompiladorASC_v01 {
         } catch (FileNotFoundException ex) {
             Logger.getLogger(CompiladorASC_v01.class.getName()).log(Level.SEVERE, null, ex);
             System.err.print("The file couldn't found");
+        }*/
+        
+        File file = new File("S_I_MC68.txt");
+        
+        try {
+            FileProcessor process = new FileProcessor(file);
+            
+            /*Se van probando comandos uno a uno para ver que se valide que se encuentran dentro del archivo,
+            con esto ya tenemos la parte del error 004: Mnemonico inexistente*/
+            System.out.println("¿Está 'aba'? R: "+process.processBuffer_containsMnemo("aba",file));
+            System.out.println("¿Está 'jmp'? R: "+process.processBuffer_containsMnemo("jmp",file));
+            System.out.println("¿Está 'ldaa'? R: "+process.processBuffer_containsMnemo("ldaa",file));
+            System.out.println("¿Está 'sbca'? R: "+process.processBuffer_containsMnemo("sbca",file));
+            System.out.println("¿Está 'xgdy'? R: "+process.processBuffer_containsMnemo("xgdy",file));
+
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(FileProcessor.class.getName()).log(Level.SEVERE, null, ex);
+            System.err.print("The file couldn't found");
         }
     }
+    
+    /*public static boolean validarIMM(String linea){
+        return linea.matches("^[ ]{1,}[A-Za-z]{1,5}[ ]{1,}\\#\\$([A|B|C|D|E|F]|[0-9]){1,4}$");
+        //return linea.matches("[^#]*\\#[^#]*\\$([A|B|C|D|E|F]|[0-9]){1,4}");
+    }*/
 
 }
--- CompiladorASC_v01/src/compiladorasc_v01/FileProcessor.java
+++ CompiladorASC_v01/src/compiladorasc_v01/FileProcessor.java
@@ -9,6 +9,8 @@ import java.io.IOException;
 import java.util.LinkedList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /* @author Edward Hunter */
 
@@ -20,7 +22,7 @@ public class FileProcessor { // THIS CLASS IS GOING TO BE USED AT THE MAIN FUNCT
         this.bufferFile = new BufferedReader(new FileReader(sourceCode));
     }
     
-    
+    //Este sólo lee el el archivo linea por linea
     public void processBuffer(){
         
         try {
@@ -35,6 +37,30 @@ public class FileProcessor { // THIS CLASS IS GOING TO BE USED AT THE MAIN FUNCT
     
     }
     
+    //Valida el patrón de los mnemónicos con una regex
+    public static boolean mnemoPattern(String line) {
+        Pattern pat = Pattern.compile("[a-z]{1,5}.*");//Se propone el patrón a buscar en las lineas
+        Matcher mat = pat.matcher(line);//valida que ese patrón esté ne la cadena
+    return mat.matches();//regresa un booleano si la encontró ese patrón
+    }
+    
+    //Valida que dentro de el archivo fileMnemos exista el nemónico
+    public boolean processBuffer_containsMnemo(String mnemo, File fileMnemos) throws FileNotFoundException {
+        BufferedReader bufferFile_Mnemos = new BufferedReader(new FileReader(fileMnemos));//Se crea una valiable local para el archivo porque sino marcaba error
+        boolean p = false;//bandera
+            try {
+            String line;
+            while ((line = bufferFile_Mnemos.readLine()) != null) { 
+                p = true;//Si encuentra el nemonico va a modificar la bandera
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(FileProcessor.class.getName()).log(Level.SEVERE, null, ex);
+            System.err.print("The file is damaged");
+        }
+    return p;//retorno de bandera
+    }
+    
+    //Este puede quedar inutlizado, pero valida se trata de convertir cada linea del archivo a un indice dentro de una lista
     public LinkedList manipularLineapoorLinea(LinkedList l){
         try {
             String line;
