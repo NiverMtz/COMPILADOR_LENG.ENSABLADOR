c061bc5ac203c481f2c829ac76dbf23f33aadb22
RojasCastilloOscar-RocoElWuero <usuario917@gmail.com>
2019-10-12 15:59:39 -0500

    Modo inherente aparentemente shido

--- CompiladorASC_v01/src/compiladorasc_v01/ModosDireccionamiento.java
+++ CompiladorASC_v01/src/compiladorasc_v01/ModosDireccionamiento.java
@@ -20,7 +20,7 @@ public class ModosDireccionamiento {
         if(mat.matches()) {
             return 1;
         }
-    return -1;  
+    return -1;
     }
     public static int devolverDir(String lineaArchivo) {
     return -1; 
@@ -35,6 +35,35 @@ public class ModosDireccionamiento {
     return -1;
     }
     public static int devolverInh(String lineaArchivo) {
+        /*if(lineaFuenteArchivo.matches("^ {1,}"+regex+"$") == true) {
+            System.out.println(i+lineaFuenteArchivo);//LISTO, ya reconoce la expresión shida
+        }
+        //Puede haber espacio(s) que haya escrito por accidente al final
+        if (lineaFuenteArchivo.matches("^ {1,}"+regex+" {1,}$") == true) {
+            System.out.println(i+lineaFuenteArchivo);//LISTO, ya reconoce la expresión no tan shida
+        }
+        //Asterisco pegado a la instrucción
+        //http://tutorials.jenkov.com/java-regex/index.html#escaping-characters
+        if (lineaFuenteArchivo.matches("^ {1,}"+regex+"\\*") == true) {
+            System.out.println(i+lineaFuenteArchivo);//Listo, ya encuentra el * pegado
+        }
+        //Asterisco separado con un(os) espacio(s) a la instrucción
+        if (lineaFuenteArchivo.matches("^ {1,}"+regex+"\\ {1,}\\*") == true) {
+            System.out.println(i+lineaFuenteArchivo);//Listo, ya encuentra el * separado (espacio(s))
+        }
+        //Comentario pegado
+        if (lineaFuenteArchivo.matches("^ {1,}"+regex+"\\*[A-Z|a-z|0-9||\\!|\"|\\#|\\$|\\%|\\&|\\/|\\(|\\)|\\=|\\?|\\¡|\\°|\\¨|\\´|\\+|\\*|\\{|\\[|\\}|\\]|\\,|\\;|\\.|\\:|\\-|\\_|\\ ]{1,}") == true) {
+            System.out.println(i+lineaFuenteArchivo);//LISTO, ya reconoce el comentario pegado y los caracteres especiales
+        }
+        //Comentario con un espacio antes
+        if (lineaFuenteArchivo.matches("^ {1,}"+regex+"\\ {1,}\\*[A-Z|a-z|0-9||\\!|\"|\\#|\\$|\\%|\\&|\\/|\\(|\\)|\\=|\\?|\\¡|\\°|\\¨|\\´|\\+|\\*|\\{|\\[|\\}|\\]|\\,|\\;|\\.|\\:|\\-|\\_|\\ ]{1,}") == true) {
+            System.out.println(i+lineaFuenteArchivo); //LISTO, ya reconoce el espacio
+        }*/
+        Pattern pat = Pattern.compile("^[ ]{1,}[A-Za-z]{1,5}[ |\\*([|A-Z|a-z|0-9||\\!|\"|\\#|\\$|\\%|\\&|\\/|\\(|\\)|\\=|\\?|\\¡|\\°|\\¨|\\´|\\+|\\*|\\{|\\[|\\}|\\]|\\,|\\;|\\.|\\:|\\-|\\_|\\ ]{0,})]{0,}$");
+        Matcher mat = pat.matcher(lineaArchivo);
+        if(mat.matches()) {
+            return 1;
+        }
     return -1;
     }
     public static int devolverRel(String lineaArchivo) {
